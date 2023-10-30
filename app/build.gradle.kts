68434614839fbd6877d8e90543d776fd59c0deae
Lucas Kivi <lucas@livefront.com>
2023-10-30 16:02:59 -0500

    Upgrade target and compile sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.pagerdemo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.pagerdemo"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
